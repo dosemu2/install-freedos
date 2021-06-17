c5967ed4ef02d22b363d246b075556d435c18200
Stas Sergeev <stsp@users.sourceforge.net>
2021-06-17 15:26:13 +0300

    add rmfdusr.bat

--- src/bat/insfdusr.bat
+++ src/bat/insfdusr.bat
@@ -1,3 +1,10 @@
 @echo off
 unix -s dosemu-installfreedosuserspace
+if ERRORLEVEL 1 goto failed
 if exist %USERDRV%:\bin\nul path %PATH%;%USERDRV%:\bin
+echo freedos installation completed.
+echo rmfdusr command can be used to remove installed freedos.
+goto done
+:failed
+echo freedos installation failed.
+:done
---
+++ src/bat/rmfdusr.bat
@@ -0,0 +1,8 @@
+@echo off
+deltree /y c:\appinfo
+deltree /y c:\bin
+deltree /y c:\doc
+deltree /y c:\help
+deltree /y c:\links
+deltree /y c:\net
+deltree /y c:\nls
