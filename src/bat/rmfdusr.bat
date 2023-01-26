ea43c702cc5bf0fcbb8555f9011483cc7832ee5f
Stas Sergeev <stsp@users.sourceforge.net>
2023-01-27 02:08:00 +0500

    install dn2 and terminal

--- src/bat/rmfdusr.bat
+++ src/bat/rmfdusr.bat
@@ -1,5 +1,5 @@
 @echo off
-deltree /y c:\appinfo
+deltree /y c:\apps
 deltree /y c:\bin
 deltree /y c:\doc
 deltree /y c:\help
@@ -7,3 +7,4 @@ deltree /y c:\links
 deltree /y c:\net
 deltree /y c:\nls
 deltree /y c:\sound
+deltree /y c:\appinfo
