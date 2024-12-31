7597a77842bf8ef51a015df6871d36bbec8e6efa
Stas Sergeev <stsp@users.sourceforge.net>
2025-01-01 01:01:54 +0300

    add dos32a extender
    Needed for OpenCP.

--- src/bat/dosrc.d/bin2path.bat
+++ src/bat/dosrc.d/bin2path.bat
@@ -6,3 +6,8 @@ for %%i in (%USERDRV%:\apps\*) do call add_path %%i
 rem can run out of env space doing below
 rem for %%i in (%USERDRV%:\net\*) do call add_path %%i
 rem for %%i in (%USERDRV%:\sound\*) do call add_path %%i
+
+if not exist %USERDRV%:\bin\dos32a.exe goto done
+set DOS4GPATH=%USERDRV%:\bin\dos32a.exe
+if exist %USERDRV%:\sound\opencp set CPEXTENDER=%USERDRV%:\bin\dos32a.exe
+:done
