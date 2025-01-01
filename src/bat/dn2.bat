074ffa7cc9031a1219778fdc4ef3dde703774045
Stas Sergeev <stsp@users.sourceforge.net>
2025-01-01 11:55:27 +0300

    add dn2.bat instead of setting PATH to dn2
    Hope in the future freedos will populate C:\LINKS dir.

---
+++ src/bat/dn2.bat
@@ -0,0 +1,4 @@
+@echo off
+echo starting DOS Navigator 2
+speed 3
+%USERDRV%:\apps\dn2\dn.com
--- src/bat/dosrc.d/bin2path.bat
+++ src/bat/dosrc.d/bin2path.bat
@@ -2,7 +2,7 @@
 if exist %USERDRV%:\bin\nul path %PATH%;%USERDRV%:\bin
 if exist %USERDRV%:\links\nul path %PATH%;%USERDRV%:\links
 rem below is needed because links are not populated :(
-for %%i in (%USERDRV%:\apps\*) do call add_path %%i
+rem for %%i in (%USERDRV%:\apps\*) do call add_path %%i
 rem can run out of env space doing below
 rem for %%i in (%USERDRV%:\net\*) do call add_path %%i
 rem for %%i in (%USERDRV%:\sound\*) do call add_path %%i
