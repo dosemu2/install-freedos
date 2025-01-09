cb0e1459f34581370de814517330a9db68ca5983
Stas Sergeev <stsp@users.sourceforge.net>
2025-01-09 13:56:03 +0300

    insfdusr: call bin2path.bat to populate env
    This is needed so that things like opencp can be started w/o
    having to restart dosemu first.

--- src/bat/insfdusr.bat
+++ src/bat/insfdusr.bat
@@ -1,7 +1,7 @@
 @echo off
 unix -s dosemu-installfreedosuserspace
 if ERRORLEVEL 1 goto failed
-if exist %USERDRV%:\bin\nul path %PATH%;%USERDRV%:\bin
+call %XBATDRV%:\dosrc.d\bin2path.bat
 echo freedos installation completed.
 echo rmfdusr command can be used to remove installed freedos.
 goto done
